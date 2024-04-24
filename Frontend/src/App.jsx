df1797c2d1f7e0da4bbda652a87bd5db0eea648c
saksham kulshrestha <saksham082001@gmail.com>
2024-04-24 16:04:20 +0530

    Fronttend done!!

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Home from './Components/Home'
+import { Link } from 'react-router-dom'
+import { Route,Routes } from 'react-router-dom'
+import Courses from './courses/Courses'
+import Signup from './Components/Signup'
+function App() {
+  return (
+    <>
+      <div className='dark:bg-slate-900 dark:text-white'>
+<Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course" element={<Courses />} />
+        <Route path="/signup" element={< Signup/>} />
+        </Routes>
+        </div>
+    </>
+     
+    
+  )
+}
+
+export default App
